c33fad0e4f6b978212602e3be50c0ee25fd3b47b
Mac Luis <macluis@MacBook-Pro-de-Mac.local>
2018-09-24 22:25:21 +0200

    Characters connected to redux

--- src/components/sections/characters/view.js
+++ src/components/sections/characters/view.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import styles from './styles'
+import { connect } from 'react-redux'
 
-export default class extends Component {
+class Characters extends Component {
+
+    componentDidMount() {
+        this.props.fetchHouseCharacters()
+    }
     
     render() {
         return (
-            <View style={{flex: 1, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center'}}>
-                <Text style={{color: 'white', fontWeight: 'bold' }}>PERSONAJES</Text>
+            <View style={styles.container}>
+            
             </View>
         )
     }
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        isFetching: state.characters.isFetching
+    }
+}
+
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        fetchHouseCharacters: () => {
+            console.log("fetchHouseCharacters")
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Characters)
