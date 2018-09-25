8941048ba2ddc0f6c4ffaa4bf6676ebe39983dd4
Mac Luis <macluis@MacBook-Pro-de-Mac.local>
2018-09-25 22:33:38 +0200

    CharacterAdd: TextInput added

--- src/components/sections/characterAdd/view.js
+++ src/components/sections/characterAdd/view.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import styles from './styles'
 
 export default class extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-
+                <View style={{padding: 20}}>
+                    <Text style={{color: 'white'}}>{'Nombre del personaje:'}</Text>
+                    <TextInput 
+                        onChangeText={ name => this.setState({ name }) }
+                        value={this.state.name}
+                        style={{backgroundColor: 'white'}}
+                    />
+                </View>
             </View>
         )
     }
